fc04d0b65165bfae6104a20e99c204403eec4c87
antoine comte <antoine.comte@gmail.com>
2011-12-01 14:09:01 +0100

    added spec2 specification

---
+++ src/test/scala/net/antoinecomte/jugcontest/SpecFooBarQixTest.scala
@@ -0,0 +1,49 @@
+package net.antoinecomte.jugcontest
+
+import org.specs2._
+class SpecFooBarQixTest extends SpecificationWithJUnit {
+  def is = """ 
+ Ecrivez un programme qui affiche les nombres de 1 à 100. Un nombre par ligne. Respectez les règles suivantes :
+
+ * Si le nombre est divisible par 3 ou contient 3, écrire "Foo" à la place de 3.
+ * Si le nombre est divisible par 5 ou contient 5, écrire "Bar" à la place de 5.
+ * Si le nombre est divisible par 7 ou contient 7, écrire "Qix" à la place de 7.
+
+
+""" ^
+    p ^
+    """
+Voici un exemple de rendu
+=========================
+
+  1
+  2
+  FooFoo
+  4
+  BarBar
+  Foo
+  QixQix
+  8
+  Foo
+  Bar
+  ...""" ! e1 ^
+    p ^
+    """    
+Mise à jour : clarifications sur les règles
+===========================================
+
+ * On regarde les diviseurs avant le contenu (ex: 51 -> FooBar)
+ * On regarde le contenu dans l'ordre où il apparait (ex: 53 -> BarFoo)
+ * On regarde les multiples dans l'ordre Foo, Bar puis Qix (ex: 21 -> FooQix)
+ * 13 contient 3 donc s'écrit "Foo"
+ * 15 est divisible par 3 et 5 et contient un 5 donc s'écrit "FooBarBar"
+ * 33 contient deux fois 3  et est divisible par 3 donc s'écrit "FooFooFoo"
+ """ ! e2 ^
+    end
+
+  import FooBarQix._
+  def comp(n: Int) = compute(List(n)).head
+  def e1 = List("1", "2", "FooFoo", "4", "BarBar", "Foo", "QixQix", "8", "Foo", "Bar") must_== compute(1 to 10)
+  def e2 = List("FooBar", "BarFoo", "FooQix", "Foo", "FooBarBar", "FooFooFoo") must_== compute(List(51, 53, 21, 13, 15, 33))
+
+}
