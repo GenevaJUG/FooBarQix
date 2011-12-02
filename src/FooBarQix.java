0122a482b7a932c15ddc3a1807d41ae06f006821
Gaetan Zoritchak <g.zoritchak@gmail.com>
2011-12-02 17:09:38 +0100

    Verion avec enum. Plus extensible, moins lisible, 4 fois moins performante.

--- src/FooBarQix.java
+++ src/FooBarQix.java
@@ -2,9 +2,21 @@
  */
 public class FooBarQix {
 
-    public static final String FOO = "Foo";
-    public static final String BAR = "Bar";
-    public static final String QIX = "Qix";
+    private static enum  FBQEnum{
+        FOO (3, "Foo"), BAR(5, "Bar"), QIX(7, "Qix");
+
+        private int value;
+        private String replacement;
+        private char chValue;
+
+        FBQEnum(int value, String replacement) {
+            assert value < 10;
+            this.value = value;
+            this.replacement = replacement;
+            this.chValue = String.valueOf(value).charAt(0);
+        }
+    }
+
 
 // -------------------------- INSTANCE --------------------------
 
@@ -35,32 +47,31 @@ public class FooBarQix {
     }
 
     private void appendDiviseurs() {
-        appendDiviseur(3, FOO);
-        appendDiviseur(5, BAR);
-        appendDiviseur(7, QIX);
-    }
-
-    private void appendDiviseur(int i, String foo) {
-        if (intToProcess % i == 0) {
-            sb.append(foo);
-            noFooBarQix = false;
+        for (FBQEnum fbqEnum : FBQEnum.values()) {
+            if (intToProcess % fbqEnum.value == 0) {
+                appendFBQ(fbqEnum);
+            }
         }
     }
 
     private void appendContenus() {
         String st = String.valueOf(intToProcess);
         for (char c : st.toCharArray()) {
-            appendFooBarQuixIfEquals('3', c, FOO);
-            appendFooBarQuixIfEquals('5', c, BAR);
-            appendFooBarQuixIfEquals('7', c, QIX);
+            appendFooBarQuixIfEquals(c);
         }
     }
 
-    private void appendFooBarQuixIfEquals(char c1, char c, String fooOrBarOrQix) {
-        if (c == c1){
-            sb.append(fooOrBarOrQix);
-            noFooBarQix = false;
+    private void appendFooBarQuixIfEquals(char c) {
+        for (FBQEnum fbqEnum : FBQEnum.values()) {
+            if (c == fbqEnum.chValue){
+                appendFBQ(fbqEnum);
+            }
         }
+    }
+
+    private void appendFBQ(FBQEnum fbqEnum) {
+        sb.append(fbqEnum.replacement);
+        noFooBarQix = false;
     }
 
     private void appendNumberIfNoFooBarQix() {
