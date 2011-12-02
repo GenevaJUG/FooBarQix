5ebb5e39738b58a0049a70ea4965aaa859df5bf2
nire <nicolas.remond@gmail.com>
2011-12-02 10:05:31 +0100

    add a jython testing framework and clean up the scala implementation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 My proposition for the FooBarQix kata, cf : http://www.code-story.net/2011/11/16/foobarqix.html
+
+So far, I did twos implementations that can be run this way :
+
+How to run them ?
+=================
+	$ jython fooBarQix.py
+	
+	$ scala fooBarQix.scala
+
+How to run the tests ?
+======================
+	$ jython fooBarQix.py | jython testFooBarQix.py
+	
+	$ scala fooBarQix.scala | jython testFooBarQix.py
--- fooBarQix.scala
+++ fooBarQix.scala
@@ -3,19 +3,16 @@ exec scala "$0" "$@"
 !#
 
 object HelloWorld {
-
-	val FBQ = List((3,"Foo"), (5,"Bar"), (7,"Qix"))
+	
+	val FBQs = scala.collection.immutable.SortedMap( 3->"Foo", 5->"Bar", 7->"Qix")
 	
 	def multiple(i:Int) = 
-		FBQ.map(t => if(i%t._1==0) t._2 else "").reduceLeft(_+_)
-	
-	def occurence(i:Int) = List(i/10, i%10).map( x => x match {
-			case 3 => "Foo"
-			case 5 => "Bar"
-			case 7 => "Qix"
-			case _ => ""
-		}).reduceLeft(_+_)
-	
+		FBQs.map(t => if(i%t._1==0) t._2 else "").reduceLeft(_+_)
+
+	def occurence(i:Int) = 
+		List(i/10, i%10).map( x => if(FBQs.contains(x)) FBQs(x) else "").reduceLeft(_+_)
+
+
 	def foobarquix(i:Int) =
 		 multiple(i) + occurence(i) match {
 			case "" => i.toString
@@ -23,48 +20,8 @@ object HelloWorld {
 		}
 	
 	def main(args: Array[String]) {
-		args match {
-			case Array("-test",_) => testIt()
-			case Array(_) => ((1 to 100) map foobarquix) map println
-			case _ => println ("./fooBarQuixInYourFace.scala [-test]")
-		}
-	}
-
-	//////////// Testing part /////////////////
-	
-	def testIt() {	
-		val EXPECTED_RESULTS = List(
-			(1,"1"),
-			(2,"2"),
-			(3,"FooFoo"),
-			(4,"4"),
-			(5,"BarBar"),
-			(6,"Foo"),
-			(7,"QixQix"),
-			(8,"8"),
-			(9,"Foo"),
-			(13,"Foo"),
-			(15,"FooBarBar"),
-			(21,"FooQix"),
-			(33,"FooFooFoo"),
-			(51,"FooBar"),
-			(53,"BarFoo")
-		)
-		
-		val ok = (EXPECTED_RESULTS map (assertFbq _).tupled).reduceLeft(_&&_)
-		if(ok)
-			println("Everything is ok")
-	}
-	
-	def assertFbq(i:Int, s:java.lang.String):Boolean = { 
-		val fbq = foobarquix(i)
-		val ok = fbq == s
-		if(!ok) 
-			println ("%d supposed to return '%s' but got '%s'" format (i, s, fbq))
-		ok
-	}
-	
-	
+		((1 to 100) map foobarquix) map println
+	}	
 }
 HelloWorld.main(args)
 
