e3ba90ea78ae8da2c321992ec3fcf17c5dc6f06c
Gaetan Zoritchak <g.zoritchak@gmail.com>
2011-12-02 10:27:47 +0100

    Première version.

---
+++ src/FooBarQix.java
@@ -0,0 +1,84 @@
+/**
+ */
+public class FooBarQix {
+
+    public static final int START = 1;
+    public static final int END = 100;
+
+    public static final String FOO = "Foo";
+    public static final String BAR = "Bar";
+    public static final String QIX = "Qix";
+
+// -------------------------- INSTANCE --------------------------
+
+    StringBuilder sb = new StringBuilder();
+
+    int intToProcess;
+
+    private boolean noFooBarQix = true;
+
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    public FooBarQix(final int start, final int end) {
+        for (intToProcess = start; intToProcess < end + 1; intToProcess++) {
+            processLine();
+        }
+    }
+
+    private void processLine() {
+        appendDiviseurs();
+        appendContenus();
+        appendNumberIfNoFooBarQix();
+        newLine();
+    }
+
+    private void newLine() {
+        sb.append('\n');
+        noFooBarQix = true;
+    }
+
+    private void appendDiviseurs() {
+        appendDiviseur(3, "Foo");
+        appendDiviseur(5, "Bar");
+        appendDiviseur(7, "Qix");
+    }
+
+    private void appendDiviseur(int i, String foo) {
+        if (intToProcess % i == 0) {
+            sb.append(foo);
+            noFooBarQix = false;
+        }
+    }
+
+    private void appendContenus() {
+        String st = String.valueOf(intToProcess);
+        for (char c : st.toCharArray()) {
+            appendFooBarQuixIfEquals('3', c, FOO);
+            appendFooBarQuixIfEquals('5', c, BAR);
+            appendFooBarQuixIfEquals('7', c, QIX);
+        }
+    }
+
+    private void appendFooBarQuixIfEquals(char c1, char c, String fooOrBarOrQix) {
+        if (c == c1){
+            sb.append(fooOrBarOrQix);
+            noFooBarQix = false;
+        }
+    }
+
+    private void appendNumberIfNoFooBarQix() {
+        if (noFooBarQix)
+            sb.append(intToProcess);
+    }
+
+    @Override
+    public String toString() {
+        return sb.toString();
+    }
+
+    // --------------------------- main() method ---------------------------
+
+    public static void main(String[] args) {
+        System.out.println(new FooBarQix(START, END));
+    }
+}
---
+++ src/TestFooBarQuix.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ */
+public class TestFooBarQuix {
+
+    @Test
+    public void highLevelTests(){
+        assertIntToFooBarQix(51 , "FooBar");
+        assertIntToFooBarQix(53 , "BarFoo");
+        assertIntToFooBarQix(21 , "FooQix");
+        assertIntToFooBarQix(13 , "Foo");
+        assertIntToFooBarQix(15 , "FooBarBar");
+        assertIntToFooBarQix(33 , "FooFooFoo");
+
+    }
+
+    private void assertIntToFooBarQix(int i, String fooBar) {
+        assertEquals(new FooBarQix(i,i).toString(), fooBar+'\n');
+    }
+
+    @Test
+    public void perf(){
+        long startTime = System.currentTimeMillis();
+        new FooBarQix(1, 1000000);
+        System.out.println("1 000 000 en " + (System.currentTimeMillis() - startTime) + " ms.");
+    }
+
+
+
+}
