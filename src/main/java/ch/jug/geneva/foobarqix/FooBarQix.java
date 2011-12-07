9329db64ad61e144d0dc712e5fa7f776c5d1d1e0
f <f@franzs-MacBook-Air.local>
2011-12-07 23:52:22 +0100

    Politiquement correct après revue des autres codes ;->

--- src/main/java/ch/jug/geneva/foobarqix/FooBarQix.java
+++ src/main/java/ch/jug/geneva/foobarqix/FooBarQix.java
@@ -5,16 +5,23 @@ package ch.jug.geneva.foobarqix;
  * @since 12/5/11 10:52 PM
  */
 public class FooBarQix {
-    public String affiche(int nombre) {
-        String string = "" + nombre;
+
+    private static final String FOO = "Foo";
+    private static final String BAR = "Bar";
+    private static final String QIX = "Qix";
+
+    public String affiche(Integer nombre) {
         StringBuilder stringBuilder = new StringBuilder();
-        if (nombre % 3 == 0) stringBuilder.append("Foo");
-        if (nombre % 5 == 0) stringBuilder.append("Bar");
-        if (nombre % 7 == 0) stringBuilder.append("Qix");
+
+        if (nombre % 3 == 0) stringBuilder.append(FOO);
+        if (nombre % 5 == 0) stringBuilder.append(BAR);
+        if (nombre % 7 == 0) stringBuilder.append(QIX);
+
+        String string = nombre.toString();
         for (char character : string.toCharArray()) {
-            if (character == '5') stringBuilder.append("Bar");
-            if (character == '3') stringBuilder.append("Foo");
-            if (character == '7') stringBuilder.append("Qix");
+            if (character == '5') stringBuilder.append(BAR);
+            if (character == '3') stringBuilder.append(FOO);
+            if (character == '7') stringBuilder.append(QIX);
         }
         return stringBuilder.length() > 0 ? stringBuilder.toString() : string;
     }
